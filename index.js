dbbe9fb6bd3019208f979923295d267aac654daa
andrjre <andrjre@gmail.com>
2025-05-11 21:16:04 +0100

    Added apple and eating mechanic.

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 let player = document.querySelector("#player");
+let apple = document.querySelector("#apple")
 
-document.addEventListener("keydown", (event) => {
+console.log(`top: ${player.style.top} left: ${player.style.left}`)
+
+document.addEventListener("keydown", playerMovement = function() {
+
     const step = 50;
     const currentLeft = parseInt(window.getComputedStyle(player).left);
     const currentTop = parseInt(window.getComputedStyle(player).top);
 
+
     switch (event.key) {
         case "w":
             player.style.top = currentTop - step + "px";
+            console.log(`top: ${player.style.top} left: ${player.style.left}`)
             break;
         case "s":
             player.style.top = currentTop + step + "px";
+            console.log(`top: ${player.style.top} left: ${player.style.left}`)
             break;
         case "a":
             player.style.left = currentLeft - step + "px";
+            console.log(`top: ${player.style.top} left: ${player.style.left}`)
             break;
         case "d":
             player.style.left = currentLeft + step + "px";
+            console.log(`top: ${player.style.top} left: ${player.style.left}`)
             break;
     }
-});
+
+   apple.style.left = "500px";
+
+ if(apple.style.left == player.style.left){
+        let eatApple = function(){
+            apple.style.backgroundColor = "white"
+        }
+        eatApple()
+        console.log("got apple")
+    }
+    }
+);
+
+
+
 
