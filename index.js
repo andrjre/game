90dbb27c8e669929b89f0b3c7347c6a1d6f5e498
andrjre <andrjre@gmail.com>
2025-05-12 21:31:25 +0100

    Pear.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let player = document.querySelector("#player");
 let apple = document.querySelector("#apple")
+let pear = document.querySelector("#pear")
+
 player.style.left = "50px"
 player.style.top = "50px"
-apple.style.left = "500px";
-apple.style.top = "100px";
-console.log(` apple is at y: ${apple.style.top} + x: ${apple.style.left}`)
+
+let appleY = getComputedStyle(apple).top;
+let appleX = getComputedStyle(apple).left;
+
+let pearY =getComputedStyle(pear).top
+let pearX =getComputedStyle(pear).left
+
+console.log(` apple is at y: ${appleY} + x: ${appleX}`)
+console.log(` pear is at y: ${pearY} + x: ${pearX}`)
 
 console.log(`y: ${player.style.top} x: ${player.style.left}`)
 
@@ -14,7 +22,8 @@ document.addEventListener("keydown", playerMovement = function() {
     const currentLeft = parseInt(window.getComputedStyle(player).left);
     const currentTop = parseInt(window.getComputedStyle(player).top);
 
-
+//player movement 
+    
     switch (event.key) {
         case "w":
             player.style.top = currentTop - step + "px";
@@ -34,17 +43,21 @@ document.addEventListener("keydown", playerMovement = function() {
             break;
     }
 
-   
+//apple collision 
 
- if(apple.style.left == player.style.left  && apple.style.top == player.style.top 
- ){
-        let eatApple = function(){
-            apple.style.backgroundColor = "white"
-        }
-        eatApple()
-        console.log("got apple")
+let eat = function(fruit){
+fruit.style.backgroundColor = "white"
+console.log(`ate ${fruit.id}`)
+}
+
+if(appleX == player.style.left  && appleY == player.style.top ){
+        eat(apple)
     }
+if(pearX == player.style.left  && pearY == player.style.top ){
+        eat(pear)
     }
+}
+
 );
 
 
